66263758ef841fabe9e72209a5267e40faf68c65
zenoo97 <85465356+zenoo97@users.noreply.github.com>
2023-07-16 14:30:30 +0900

    [Updata] react-router-dom 사용
    Router, Route, Link 설정

--- market-web/src/App.js
+++ market-web/src/App.js
@@ -1,10 +1,23 @@
 import "./App.css";
 import MainPage from "./main";
+import { Route, Switch } from "react-router-dom";
+import ProductPage from "./product";
+import UploadPage from "./upload";
 
 function App() {
   return (
     <div>
-      <MainPage />
+      <Switch>
+        <Route exact={true} path={"/"}>
+          <MainPage />
+        </Route>
+        <Route exact={true} path={"/products/:id"}>
+          <ProductPage />
+        </Route>
+        <Route exact={true} path={"/upload"}>
+          <UploadPage />
+        </Route>
+      </Switch>
     </div>
   );
 }
