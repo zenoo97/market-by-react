29d8f036a917d36cacab0af27ab2f172d0f53a8c
zenoo97 <85465356+zenoo97@users.noreply.github.com>
2023-07-16 15:40:49 +0900

    Revert "[Updata] react-router-dom 사용"
    This reverts commit 66263758ef841fabe9e72209a5267e40faf68c65.

--- market-web/src/App.js
+++ market-web/src/App.js
@@ -1,23 +1,10 @@
 import "./App.css";
 import MainPage from "./main";
-import { Route, Switch } from "react-router-dom";
-import ProductPage from "./product";
-import UploadPage from "./upload";
 
 function App() {
   return (
     <div>
-      <Switch>
-        <Route exact={true} path={"/"}>
-          <MainPage />
-        </Route>
-        <Route exact={true} path={"/products/:id"}>
-          <ProductPage />
-        </Route>
-        <Route exact={true} path={"/upload"}>
-          <UploadPage />
-        </Route>
-      </Switch>
+      <MainPage />
     </div>
   );
 }
