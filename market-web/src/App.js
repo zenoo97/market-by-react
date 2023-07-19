ce23c11dc34b967ea739d9afd4faf7cdcd10b0f5
zenoo97 <85465356+zenoo97@users.noreply.github.com>
2023-07-19 17:55:52 +0900

    Revert "[Update] 상품 업로드 완성 (by antd)"
    This reverts commit 25095d9b5e5c0a532facd6855a114a7303227407.

--- market-web/src/App.js
+++ market-web/src/App.js
@@ -14,6 +14,7 @@ function App() {
           <Link to={"/"}>
             <img src="./images (2)/images/icons/logo.png" alt="" />
           </Link>
+
           <Button
             size="20"
             onClick={() => {
